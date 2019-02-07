de252325467d3367a772498979ac3d3d67d3fa61
Talisa Erazo <talisaerazo@gmail.com>
2019-02-07 13:53:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   def valid?
     sum_one_two = @triangle_sides[0] + @triangle_sides[1]
     sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three - @triangle_sides[1] + @triangle_sides[2]
+    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
 
     if (@triangle_sides.none? {|side| side <= 0}) &&
       (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
@@ -23,7 +23,7 @@ class Triangle
     if valid?
       if @triangle_sides.uniq.length == 1
         return :equilateral
-      else @triangle_sides.uniq.length == 2
+      elsif @triangle_sides.uniq.length == 2
         return :isosceles
       else
         return :scalene
